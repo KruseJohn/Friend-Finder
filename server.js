5b7d58d3d8f2a565ced1874d34641eaaa476ad69
John <kruse67@live.com>
2019-02-24 06:12:35 -0700

    server.js done

--- server.js
+++ server.js
@@ -1,3 +1,17 @@
+var express = require("express");
+var bodyParser = require("body-parser")
+var path = require("path");
+
+var app = express();
 var PORT = process.env.PORT || 3000;
 
-app.listen(PORT, function() {});
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+app.use(bodyParser.text());
+
+require("./app/routing/apiRouting")(app);
+require("./app/routing/htmlRouting")(app);
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT: " + PORT);
+});
